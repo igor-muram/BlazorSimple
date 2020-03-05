ac7cb982cad3ee60cf790527a11a03f07b572165
Nikita <nikita.nstu@gmail.com>
2020-03-06 00:42:48 +0700

    Orbit Controls for cube

--- BlazorSimple/wwwroot/js/interop.js
+++ BlazorSimple/wwwroot/js/interop.js
@@ -1,38 +1,54 @@
-﻿
-
-function add() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
-}
-
-function remove() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(10, 10, 100, 100);
-}
+﻿var scene, camera, renderer;
+var isCubeActive = false;
+var cube;
 
 function init() {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000)
+    camera.position.z = 5;
 
-    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(1000, 1000);
     document.getElementById('myCanvas').appendChild(renderer.domElement)
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1)
-    var material = new THREE.MeshBasicMaterial({ color: 0xff0051 })
-    var cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
-    renderer.render(scene, camera)
-    camera.position.z = 5
+    cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+    cameraControls.target.set(0, 0, 0);
+    cameraControls.update();
+}
 
-    function animate() {
-        requestAnimationFrame(animate)
+function addCube() {
+
+    if (cube === undefined) {
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({ color: 0xff0051 });
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+    }
+    else {
+        scene.add(cube);
+    }
+}
+
+function gameloop() {
+    requestAnimationFrame(gameloop)
+
+    if (isCubeActive) {
         cube.rotation.x += 0.04;
         cube.rotation.y += 0.04;
-        renderer.render(scene, camera)
     }
-    animate()
+
+    renderer.render(scene, camera)
+}
+
+function start() {
+    isCubeActive = true;
+}
+
+function stop() {
+    isCubeActive = false;
+}
+
+function removeCube() {
+    scene.remove(cube);
 }
